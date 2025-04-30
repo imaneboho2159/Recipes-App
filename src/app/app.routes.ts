80c166eaa42e97bedd5c08028420e7cbd953a742
imaneboho2159 <imaboh21@gmail.com>
2025-04-30 15:14:08 +0100

    display all the recipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { RecipeListComponent } from './features/recipe-list/recipe-list.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent },
+    {path: 'menu', component: RecipeListComponent},
     // {path: 'recipe/:id',component : RecipesDetailComponent}
 ];
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  template: `
+    <header class="flex justify-between items-center px-8 py-6 bg-green-900 text-white">
+      <h1 class="text-3xl font-bold">FOODY</h1>
+      <nav class="space-x-8 text-lg font-light italic">
+        <a routerLink="/home" routerLinkActive="text-yellow-300" class="hover:text-yellow-300 cursor-pointer">Home</a>
+        <a routerLink="/menu" routerLinkActive="text-yellow-300" class="hover:text-yellow-300 cursor-pointer">Menu</a>
+        <a routerLink="/about" routerLinkActive="text-yellow-300" class="hover:text-yellow-300 cursor-pointer">About</a>
+        <a routerLink="/shop" routerLinkActive="text-yellow-300" class="hover:text-yellow-300 cursor-pointer">Shop</a>
+      </nav>
+    </header>
+  `,
+ styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
